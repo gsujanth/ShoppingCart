2bdd80e81e561ede9fd22d58075e868c41cac889
gsujanth <>
2019-03-14 01:07:45 -0400

    second commit

--- src/test/java/CartTest.java
+++ src/test/java/CartTest.java
@@ -1,25 +1,42 @@
 import org.junit.*;
 
+import java.util.ArrayList;
+
 public class CartTest {
 
-    private Cart cart=new Cart();
+    private Cart cart;
+    private Item item;
+    private ArrayList<Item> items;
 
     @Before
+    public void initializeCart(){
+        //System.out.println("hi");
+        cart=new Cart();
+    }
+
+    @After
+    public void destroy(){
+        //System.out.println("bye");
+        cart.itemList.clear();
+        Cart.totalPrice=0.0;
+    }
+
     @Test
     public void cartEmptyOrNot(){
         Assert.assertEquals("Is Cart Empty or Not?",0,cart.itemList.size());
     }
 
-    @After
-    public void destroy(){
-        cart.itemList.clear();
-        Cart.totalPrice=0.0;
-    }
     @Test
     public void getItemsTest(){
-        cart.addItem(new Item("Handbag",500,false),5);
+        items=new ArrayList<>();
+        item=new Item("Handbag",500,false);
+        cart.addItem(item,5);
+        items.add(item);
         Assert.assertEquals("Get Items Test",new Item("Handbag",500,false).toString(),cart.getItems().get(0).toString());
-        cart.addItem(new Item("Watch",300,false),5);
+        item=new Item("Watch",300,false);
+        cart.addItem(item,5);
+        items.add(item);
+        Assert.assertEquals("Get Items Test 2",items.toString(),cart.getItems().toString());
     }
 
     @Test
@@ -31,7 +48,7 @@ public class CartTest {
 
     @Test
     public void itemQuantitiesTest(){
-//
+
     }
 
 }
